3f67ad56efab1683a59466cc570545bf818af320
LothlanEmbers <psheldon93@gmail.com>
2018-10-25 19:51:27 -0700

    added some character stuff

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,16 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+[Serializable]
+public class Character : MonoBehaviour
+{
+    public string charName;
+    public int level;
+    public int exp;
+   
 }
---
+++ Assets/Scripts/SaveCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Character newCharacter = new Character();
+        newCharacter.charName = null;
+
+
+        string json = JsonUtility.ToJson(newCharacter);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
