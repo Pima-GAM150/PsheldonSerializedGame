d57be85d75855365471568850d06beb72c4f7354
Psheldon <psheldon@mail.pima.edu>
2018-11-01 19:38:14 -0700

    Added town scene, movement script, player prefab, and basic sprite.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 currentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+
+        float newVelocityX = 0f;
+        if (moveHorizontal < 0 && currentVelocity.x <= 0)
+        {
+            newVelocityX = -speed;
+        } else if (moveHorizontal > 0 && currentVelocity.x >= 0)
+        {
+            newVelocityX = speed;
+        }
+
+        float newVelocityY = 0f;
+        if (moveVertical < 0 && currentVelocity.y <= 0)
+        {
+            newVelocityY = -speed;
+        } else if (moveVertical > 0 && currentVelocity.y >= 0)
+        {
+            newVelocityY = speed;
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(newVelocityX, newVelocityY);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UnitStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour {
+
+    public string unitName;
+    public float health;
+    public float mana;
+    public float attack;
+    public float magic;
+    public float defense;
+    public float mDefense;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
